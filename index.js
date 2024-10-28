0fbd0a7a5702c502725ed4a4de4f3e9dce64bf8e
Sudharsan1002 <sudharsanselva2003@gmail.com>
2024-10-28 10:22:52 +0530

    ADDED ENVIRONMENT VARIABLES

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 const Express = require("express");
 const { createDbConnection } = require("./db");
 
-const RecipeController=require("./controllers/Recipes.Controller")
+const RecipeController = require("./controllers/Recipes.Controller");
 
+// ENVIRONMENT CONFIGURATION
+require("dotenv").config();
 
 // CREATE AN API SERVER
 const API_SERVER = Express();
 
-
 // PARSING INCOMING REQUEST BODY AS JSON
 API_SERVER.use(Express.json());
-
 
 // CREATE DB CONNECTION
 createDbConnection();
 
-
-
-
 // INJECT ROUTERS
 API_SERVER.use("/recipes", RecipeController);
 
-
 //  START AND LISTEN INCOMING REQUEST TO THIS SERVER
-API_SERVER.listen(3000, "localhost", function () {
+API_SERVER.listen(process.env.PORT, process.env.HOSTNAME, function () {
   console.log("Server started");
-  console.log("http://localhost:3000");
+  console.log(`http://${process.env.HOSTNAME}:${process.env.PORT}`);
 });
