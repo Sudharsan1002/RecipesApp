78fe24b81eec42441366ceed1f6ebd63dfe096a7
Sudharsan1002 <sudharsanselva2003@gmail.com>
2024-10-28 12:04:45 +0530

    2

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGODBURI = "mongodb://127.0.0.1:27017/test";
+const MONGODBURI = "mongodb://127.0.0.1:27017/";
 
 // FUNCTION TO CREATE DATABASE CONNECTIVITY
 async function createDbConnection() {
